24758b4129dc756351f8e29583fd686e1ad8663a
ctghadiri <ctghadiri@gmail.com>
2019-02-25 21:47:50 -0800

    Alerts, Prompts and Writing to DOM 2

--- 03Javascript/04-core-js-user-input-and-dom-2/challenge-prompt.js
+++ 03Javascript/04-core-js-user-input-and-dom-2/challenge-prompt.js
@@ -4,24 +4,28 @@
 
 // Using the `confirm` method, welcome the user to the Boba Shop and ask them if they would like a boba
 // Store the user response in a variable `wantsBoba`
-
+var wantsBoba = confirm("Welcome to the Boba Shop. Would you like a boba?");
 
 // Create an if statement checking whether the user wanted boba from the earlier `confirm` dialogue
-
-
-
+if (wantsBoba === true) {
+    
+    
     // Declare a variable `drinkFlavor`
-
+    var drinkFlavor;
     
     // Prompt the user for what flavor they would like for their drink
     // Store their response into the variable `drinkFlavor`
-
     
+    drinkFlavor = prompt("What flavor would you like?");
+    document.write("Your " + drinkFlavor + " will be ready soon!");
+}
     
     // Write to the DOM a message to the user telling them that their drink will be ready soon.
     // Use string concatenation in order to specify the flavor of their drink
 
     
 // Else, write "Goodbye! Come back soon!" to the DOM
-
+else {
+    document.write("Goodbye! Come back soon!");
+}
 
