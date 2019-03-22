1afca889f306e4c9d95481c9a34801f766765874
ctghadiri <ctghadiri@gmail.com>
2019-03-21 20:27:21 -0700

    09node code drills complete

--- 09NodeJs/02-core-node-fizzbuzz/prompt.js
+++ 09NodeJs/02-core-node-fizzbuzz/prompt.js
@@ -1,7 +1,23 @@
+var n = process.argv[2]
 
 function fizzBuzz(n) {
 // ===================== YOUR WORK HERE =====================
-
+    for(var i = 0; i < n-1; i++){
+        if((i % 3 ===0) && (i % 5 === 0)){
+            console.log("fizzbuzz")
+        }
+        else{
+            if(i % 3 === 0){
+                console.log("fizz");
+            }
+            else if(i % 5 === 0){
+                console.log("buzz")
+            }
+            else{
+                console.log(i)
+            }
+        }
+    }
 
 
 
